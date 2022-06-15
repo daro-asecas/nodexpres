525d01a9a7513cc34ac41f44fbbfcb3a07dd5f6e
Daro <daro.1919@gmail.com>
2022-06-15 18:57:18 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+// motor de plantillas
+app.set('view engine','ejs');
+app.set('views', __dirname + "/views");
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+  res.render("index", {titulo : "mi titulo dinamico"})
+});
+
+app.get('/index', (req, res) => {
+  res.render("index", {titulo : "mi titulo dinamico"})
+});
+
+app.get('/servicios', (req, res) => {
+  res.render("servicios", {titulo : "mi titulo de servicios"})
+});
+
+app.get('/contacto', (req, res) => {
+  res.render("contacto", {titulo : "titulo de contacto"})
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404",{
+    titulo: "404",
+    descripcion: "Error 404"
+  })
+});
+
+//
+// app.get("/", (req, res) => {
+//   res.send("Mi respuesta desde express")
+// });
+
+app.listen(port, () => {
+  console.log(`Servidor a su servicio en el puerto: ${port}`);
+});
